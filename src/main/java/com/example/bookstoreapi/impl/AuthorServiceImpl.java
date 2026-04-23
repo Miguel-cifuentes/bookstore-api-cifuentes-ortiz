a6c28aadcdf23781a41591ae17cbd0f1fe1080da
Miguel Cifuentes <miguelcifuentes@ustavillavo.edu.co>
2026-04-23 01:22:02 -0500

    se agrego archivos correspondientes al feature/author-category

--- src/main/java/com/example/bookstoreapi/impl/AuthorServiceImpl.java
+++ src/main/java/com/example/bookstoreapi/impl/AuthorServiceImpl.java
@@ -1,4 +1,67 @@
 package com.example.bookstoreapi.impl;
 
-public class AuthorServiceImpl {
+import com.example.bookstoreapi.dto.request.AuthorRequest;
+import com.example.bookstoreapi.dto.response.AuthorResponse;
+import com.example.bookstoreapi.entity.Author;
+import com.example.bookstoreapi.exception.custom.AuthorHasBooksException;
+import com.example.bookstoreapi.exception.custom.ResourceNotFoundException;
+import com.example.bookstoreapi.mapper.AuthorMapper;
+import com.example.bookstoreapi.repository.AuthorRepository;
+import com.example.bookstoreapi.repository.BookRepository;
+import com.example.bookstoreapi.service.AuthorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AuthorServiceImpl implements AuthorService {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+    private final AuthorMapper authorMapper;
+
+    @Override
+    public AuthorResponse create(AuthorRequest request) {
+        Author author = authorMapper.toEntity(request);
+        return authorMapper.toResponse(authorRepository.save(author));
+    }
+
+    @Override
+    public AuthorResponse update(Long id, AuthorRequest request) {
+        Author author = authorRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Autor no encontrado"));
+
+        author.setName(request.getName());
+        return authorMapper.toResponse(authorRepository.save(author));
+    }
+
+    @Override
+    public AuthorResponse getById(Long id) {
+        Author author = authorRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Autor no encontrado"));
+        return authorMapper.toResponse(author);
+    }
+
+    @Override
+    public List<AuthorResponse> getAll() {
+        return authorRepository.findAll()
+                .stream()
+                .map(authorMapper::toResponse)
+                .toList();
+    }
+
+    @Override
+    public void delete(Long id) {
+        Author author = authorRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Autor no encontrado"));
+
+        boolean hasBooks = bookRepository.existsByAuthor(author);
+        if (hasBooks) {
+            throw new AuthorHasBooksException("No se puede eliminar el autor porque tiene libros asociados");
+        }
+
+        authorRepository.delete(author);
+    }
 }
