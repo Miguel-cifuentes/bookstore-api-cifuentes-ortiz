127f1e058e7ded3a13b1c73fd3f8b216249c5988
millosviasus-design <millosviasus@gmail.com>
2026-04-23 01:21:54 -0500

    se agrego archivos correspondientes al feature/book-catalog

--- src/main/java/com/example/bookstoreapi/impl/BookServiceImpl.java
+++ src/main/java/com/example/bookstoreapi/impl/BookServiceImpl.java
@@ -1,4 +1,74 @@
 package com.example.bookstoreapi.impl;
+import com.example.bookstoreapi.dto.request.BookRequest;
+import com.example.bookstoreapi.dto.response.BookResponse;
+import com.example.bookstoreapi.entity.Author;
+import com.example.bookstoreapi.entity.Book;
+import com.example.bookstoreapi.entity.Category;
+import com.example.bookstoreapi.mapper.BookMapper;
+import com.example.bookstoreapi.repository.AuthorRepository;
+import com.example.bookstoreapi.repository.BookRepository;
+import com.example.bookstoreapi.repository.CategoryRepository;
+import com.example.bookstoreapi.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 
 public class BookServiceImpl {
+    private final BookRepository bookRepository;
+    private final AuthorRepository authorRepository;
+    private final CategoryRepository categoryRepository;
+
+    //  CREAR LIBRO
+    @Override
+    public BookResponse create(BookRequest request) {
+
+        //  Buscar autor
+        Author author = authorRepository.findById(request.getAuthorId())
+                .orElseThrow(() -> new RuntimeException("Autor no encontrado"));
+
+        //  Buscar categorías
+        List<Category> categories = categoryRepository.findAllById(request.getCategoryIds());
+
+        //  Construir entidad
+        Book book = new Book();
+        book.setTitle(request.getTitle());
+        book.setIsbn(request.getIsbn());
+        book.setPrice(request.getPrice());
+        book.setStock(request.getStock());
+        book.setAuthor(author);
+        book.setCategories(categories);
+
+        // Guardar y mapear
+        return BookMapper.toResponse(bookRepository.save(book));
+    }
+
+    // LISTAR TODOS
+    @Override
+    public List<BookResponse> findAll() {
+        return bookRepository.findAll()
+                .stream()
+                .map(BookMapper::toResponse)
+                .toList();
+    }
+
+    // FILTRAR POR AUTOR
+    @Override
+    public List<BookResponse> findByAuthor(Long authorId) {
+        return bookRepository.findByAuthorId(authorId)
+                .stream()
+                .map(BookMapper::toResponse)
+                .toList();
+    }
+
+    @Override
+    public List<BookResponse> findByCategory(Long categoryId) {
+        return bookRepository.findByCategoriesId(categoryId)
+                .stream()
+                .map(BookMapper::toResponse)
+                .toList();
+    }
 }
